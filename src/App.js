ba18eb2f95db4f6b642b5e57315d498c002fe822
yunjungheo <ggomi7900@gamil.com>
2022-03-30 23:19:02 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail"
+import Home from "./routes/Home"
+
+function App() {
+ return (
+  <Router>
+    <Switch>
+      <Route path="/movie">
+      <Detail />
+      </Route>
+      <Route path="/">
+      <Home />
+      </Route>
+    </Switch>
+  </Router>
+ )
+}
+
+export default App;
